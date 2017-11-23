1a752c6a191472bea09d4e9c62a5603d8d5acc97
PolCarCat <polcarcat@gmail.com>
2017-11-23 11:12:17 +0100

    return

--- Motor2D/Label.cpp
+++ Motor2D/Label.cpp
@@ -14,20 +14,20 @@ Label::~Label()
 
 bool Label::Start()
 {
-
+	return true;
 }
 
 bool Label::PreUpdate()
 {
-
+	return true;
 }
 
 bool Label::PostUpdate()
 {
-
+	return true;
 }
 
 bool Label::CleanUp()
 {
-
+	return true;
 }
