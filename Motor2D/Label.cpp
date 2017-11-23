7973284cc52d15471247bb759209d50b4b6d2340
marc094 <marcmoreno2@gmail.com>
2017-11-23 12:21:58 +0100

    More label WIP

--- Motor2D/Label.cpp
+++ Motor2D/Label.cpp
@@ -2,13 +2,18 @@
 
 
 
-Label::Label()
+Label::Label() : InterfaceElement()
 {
 }
 
+Label::Label(const char* font_path, int pSize) : InterfaceElement()
+{
+	font = TTF_OpenFont(font_path, pSize); // Needs to be closed (?)
+}
 
 Label::~Label()
 {
+
 }
 
 
@@ -30,4 +35,52 @@ bool Label::PostUpdate()
 bool Label::CleanUp()
 {
 
+}
+
+void Label::setAlignment(Label::Alignment alignment)
+{
+	this->alignment = alignment;
+}
+
+Label::Alignment Label::getAlignment() const
+{
+	return alignment;
+}
+
+void Label::setText(const char* string, ...)
+{
+	if (string != NULL)
+	{
+		static va_list  ap;
+
+		// Construct the string from variable arguments
+		va_start(ap, string);
+		this->string.create(string, ap);
+		va_end(ap);
+	}
+}
+
+void Label::setText(p2SString string)
+{
+	this->string = string;
+}
+
+void Label::getString(const char* string) const
+{
+	string = this->string.GetString();
+}
+
+void Label::getString(p2SString& string) const
+{
+	string = this->string;
+}
+
+void Label::setFont(Font* font)
+{
+	this->font = font;
+}
+
+Font* Label::getFont()
+{
+	return font;
 }
--- Motor2D/Label.h
+++ Motor2D/Label.h
@@ -2,11 +2,20 @@
 #include "InterfaceElement.h"
 #include "SDL_ttf\include\SDL_ttf.h"
 
+typedef _TTF_Font Font;
+
 class Label :
 	public InterfaceElement
 {
 public:
+	enum Alignment {
+		CENTERED,
+		LEFT,
+		RIGHT
+	};
+
 	Label();
+	Label(const char* font_path, int pSize);
 	~Label();
 
 	bool Start() override;
@@ -14,7 +23,17 @@ public:
 	bool PostUpdate() override;
 	bool CleanUp() override;
 
-private:
-	_TTF_Font* font;
-};
+	void setAlignment(Alignment alignment);
+	Alignment getAlignment() const;
+	void setText(const char* string, ...);
+	void setText(p2SString string);
+	void getString(const char* string) const;
+	void getString(p2SString& string) const;
+	void setFont(Font* font);
+	Font* getFont();
 
+private:
+	Font* font = nullptr;
+	p2SString string;
+	Alignment alignment;
+};
