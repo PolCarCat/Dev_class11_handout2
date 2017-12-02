0689b4bb9b7a1189ec23d5b1dc2ccc2767310bfa
DESKTOP-Marc <marcmoreno2@gmail.com>
2017-12-02 20:34:47 +0100

    Arreglat el drag de la window

--- Motor2D/Window.cpp
+++ Motor2D/Window.cpp
@@ -1,5 +1,6 @@
 #include "Window.h"
 #include "j1Input.h"
+#include "j1Window.h"
 
 
 Window::Window(uint _x, uint _y, SDL_Texture* _tex, bool _enabled, SDL_Rect* _anim) : Sprite(_x,_y, _tex, _enabled, _anim)
@@ -22,11 +23,22 @@ bool Window::PostUpdate()
 	SDL_Rect result;
 	if (SDL_IntersectRect(&rect, &Mouse, &result) == SDL_TRUE)
 	{
-		if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_REPEAT && prev_mouse.x != Mouse.x && prev_mouse.y != Mouse.y)
+		if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_DOWN)
 		{
-			DragWindow();
+			locked = true;
+			iPoint p = { rect.x, rect.y }, m = { Mouse.x, Mouse.y };
+			delta_pos_mouse = p - m;
 		}
 	}
+
+	if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_REPEAT && locked == true/*prev_mouse.x != Mouse.x && prev_mouse.y != Mouse.y*/)
+	{
+		//DragWindow();
+		rect.x = Mouse.x + delta_pos_mouse.x;
+		rect.y = Mouse.y + delta_pos_mouse.y;
+	}
+	else if (App->input->GetMouseButtonDown(SDL_BUTTON_LEFT) == KEY_UP && locked == true)
+		locked = false;
 
 	current_anim = &idle_anim;
 	App->render->Blit(tex, rect.x, rect.y, false, current_anim);
@@ -39,4 +51,57 @@ void Window::DragWindow()
 	App->input->GetMouseMotion(mousemove.x, mousemove.y);
 	rect.x += mousemove.x;
 	rect.y += mousemove.y;
+}
+
+Sprite* Window::AddSprite(uint x, uint y, SDL_Texture* tex, bool enabled, SDL_Rect* anim)
+{
+	Sprite* aux = new Sprite(x, y, tex, enabled, anim);
+
+	elements.add(aux);
+	return aux;
+}
+
+Label* Window::AddLabel(int x, int y, int psize, const char * font_path, SDL_Color color, Label::RenderMode mode, const char* format, ...)
+{
+	Label* aux = new Label(x, y, font_path, psize, mode);
+	aux->setColor(color);
+
+	if (format != NULL)
+	{
+		va_list  ap;
+		char buffer[TMP_STRING_SIZE];
+
+		va_start(ap, format);
+		int res = vsprintf_s(buffer, TMP_STRING_SIZE, format, ap);
+		va_end(ap);
+
+		if (res > 0) {
+			aux->setString(buffer);
+		}
+	}
+	elements.add(aux);
+
+	return aux;
+}
+
+Button* Window::AddButton(uint _x, uint _y, SDL_Texture* _tex, bool _enabled, SDL_Rect* _anim, Callback_c callback,
+	SDL_Rect* _hovered_anim, SDL_Rect* _pressed_anim, const char* font_path, int pSize, Label::RenderMode mode)
+{
+	Button* aux = new Button(_x, _y, _tex, _enabled, _anim, callback, _hovered_anim, _pressed_anim);
+
+	if (_anim != nullptr) {
+		_x += _anim->w / 2;
+		_y += _anim->h / 2;
+	}
+	else if (_tex != nullptr) {
+		int w, h;
+		SDL_QueryTexture(_tex, nullptr, nullptr, &w, &h);
+		_x += w / 2;
+		_y += h / 2;
+	}
+
+	Label* label = new Label(_x, _y, font_path, pSize, mode);
+	aux->setLabel(label);
+	elements.add(aux);
+	return aux;
 }
--- Motor2D/Window.h
+++ Motor2D/Window.h
@@ -6,13 +6,25 @@ class Window :
 	public Sprite
 {
 public:
-	p2List<Button*> buttons;
+	/*p2List<Button*> buttons;
 	p2List<Label*> labels;
+	p2List<Sprite*> sprites;*/
+	p2List<IE*> elements;
 	Window(uint _x = 0, uint _y = 0, SDL_Texture* _tex = nullptr, bool _enabled = false, SDL_Rect* _anim = nullptr);
 	~Window();
+
 	void DragWindow();
 	bool PostUpdate();
+
+	Sprite* AddSprite(uint x, uint y, SDL_Texture* tex, bool enabled = true, SDL_Rect* anim = NULL);
+	Label* AddLabel(int x, int y, int psize, const char* font_path, SDL_Color color, Label::RenderMode mode, const char* format, ...);
+	Button* AddButton(uint _x, uint _y, SDL_Texture* _tex, bool _enabled, SDL_Rect* _anim, Callback_c callback, SDL_Rect* _hovered_anim = nullptr,
+		SDL_Rect* _pressed_anim = nullptr, const char* font_path = nullptr, int pSize = -1, Label::RenderMode mode = Label::BLENDED);
+
+
 	SDL_Rect Mouse;
 	SDL_Rect prev_mouse;
+	bool locked = false;
+	iPoint delta_pos_mouse;
 };
 
